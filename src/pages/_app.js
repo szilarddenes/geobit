e0ede0e6bdf65c9e3a8369eebace51c2892d5e0e
sd <szilard.denes.geo@gmail.com>
2025-03-12 16:45:18 +0100

    Update _app.js with enhanced styling and meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,34 @@
 import '@/styles/globals.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        {/* Include Inter font from Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        
+        {/* Default meta tags */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="GeoBit - TLDR newsletter for geoscientists with the latest research, industry news, and developments in earth sciences" />
+      </Head>
       <Component {...pageProps} />
-      <ToastContainer position="bottom-right" />
+      <ToastContainer 
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
-  )
+  );
 }
