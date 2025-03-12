4fbada19f73cd9a42b18032d0790bd605915b639
sd <szilard.denes.geo@gmail.com>
2025-03-12 14:12:54 +0100

    Add Next.js configuration file

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/dashboard',
+        permanent: true,
+      },
+    ];
+  },
+  // For Firebase deployment
+  distDir: '.next',
+};
+
+module.exports = nextConfig;
